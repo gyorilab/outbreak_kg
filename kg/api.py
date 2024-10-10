d8c13e261357b5ab939793cb30f6fed5a2483f39
nanglo123 <tnanglo1998@gmail.com>
2024-10-10 10:59:02 -0400

    Initial implementation of neo4j client and endpoint to query the graph

---
+++ kg/api.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from client import client
+
+import neo4j
+
+app = Flask(__name__)
+
+
+@app.route("/search", methods=["POST"])
+def search():
+    disease = request.json.get("disease")
+    geolocation = request.json.get("geolocation")
+    pathogen = request.json.get("pathogen")
+    timestamp = request.json.get("timestamp")
+    symptom = request.json.get("symptom")
+
+    search_results = client.query_graph(disease, geolocation, pathogen,
+                                      timestamp, symptom)
+    return_value = {}
+    for path_index, path in enumerate(search_results):
+        return_value[path_index] = []
+        for path_compartment in path:
+            if isinstance(path_compartment, neo4j.graph.Node):
+                dict_path_compartment = dict(path_compartment)
+                return_value[path_index].append(dict_path_compartment)
+            elif path_compartment is not None:
+                return_value[path_index].append(path_compartment.type)
+
+    return jsonify(return_value)
